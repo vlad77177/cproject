439c4f29776cdc0ad9cab457065e418e3e7dab6d
Владислав Кособоков < >
2016-12-01 16:40:53 +0300

    Added: Practic2_task3

---
+++ Practic2_task3/Practic2_task3/practic2_task3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+	const int increment=2;
+
+	int strings;
+
+	printf("Enter the number of strings: ");
+	scanf("%d",&strings);
+
+	int indent=strings-increment;
+	int stars=1;
+
+	for(int str=1;str<=strings;str++,indent--,stars=stars+increment){
+		for(int i=0;i<=indent;i++)
+			printf(" ");
+		for(int s=0;s<stars;s++)
+			printf("%c",'*');
+		printf("\n");
+	}
+
+	int end;
+	scanf("%d",&end);
+}
