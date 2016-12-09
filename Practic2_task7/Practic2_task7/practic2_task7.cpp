e58da00e3bc1e513b80012d9a2a72a5d686653b1
Владислав Кособоков < >
2016-12-09 21:37:40 +0300

    Hotfix

--- Practic2_task7/Practic2_task7/practic2_task7.cpp
+++ Practic2_task7/Practic2_task7/practic2_task7.cpp
@@ -21,22 +21,17 @@ int main(){
 	int size=strlen(string);
 
 	for(int i=0;i<size;i++){
-		printf("%s%d, %s%c\n","Iteration=",i,"symbol=",string[i]);
 		TABLECELL* pnow=start;
 		bool newch=true;
 		for(int j=0;j<tablesize;j++){
 			char comp=pnow->ch;
-			printf("%s%d %s%c\n","j-iteration",j,"char comp=",pnow->ch);
 			if(string[i]==comp){
-				printf("ch==comp!\n");
 				pnow->frequency++;
 				newch=false;
-				printf("%d%s%s%c%s%d\n",j,"-struct: ","pnow->ch=",pnow->ch," pnow->f=",pnow->frequency);
 			}
 			pnow++;
 		}
 		if(newch==true){
-			printf("%s %c\n","new symbol=",string[i]);
 			pnow=start;
 			for(int y=0;y<tablesize;y++,pnow++);
 			pnow->ch=string[i];
@@ -46,7 +41,7 @@ int main(){
 	}
 
 	for(int k=0;k<tablesize;k++){
-		printf("%c : %d : %x\n",start->ch,start->frequency,&start->ch);
+		printf("%c : %d\n",start->ch,start->frequency);
 		start++;
 	}
 
